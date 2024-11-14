534f10c481e9a514ede9df44b597f71b1df22bd5
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2024-11-14 07:01:03 -0300

    fix lint

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
 "use client"
 
 // Inspired by react-hot-toast library
@@ -18,6 +20,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
