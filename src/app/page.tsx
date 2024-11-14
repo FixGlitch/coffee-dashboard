51f67c4a689771c125e8976dc07331ef2c2e6649
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2024-11-14 07:48:10 -0300

    fix app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
+// "use client";
+
+// import { useAuth } from "@/hooks/useAuth";
+// import LandingLayout from "./(landing)/layout";
+// import DashboardLayout from "./dashboard/layout";
+// import LandingPage from "./(landing)/page";
+// import DashboardPage from "./dashboard/page";
+
+// export default function HomePage() {
+//   const { isLoggedIn } = useAuth();
+
+//   return (
+//     <main className="no-scrollbar overflow-hidden">
+//       {isLoggedIn ? (
+//         <DashboardLayout>
+//           <DashboardPage />
+//         </DashboardLayout>
+//       ) : (
+//         <LandingLayout>
+//           <LandingPage />
+//         </LandingLayout>
+//       )}
+//     </main>
+//   );
+// }
+
 "use client";
 
 import { useAuth } from "@/hooks/useAuth";
 import LandingLayout from "./(landing)/layout";
-import DashboardLayout from "./dashboard/layout";
 import LandingPage from "./(landing)/page";
-import DashboardPage from "./dashboard/page";
 
 export default function HomePage() {
-  const { isLoggedIn } = useAuth();
-
   return (
     <main className="no-scrollbar overflow-hidden">
-      {isLoggedIn ? (
-        <DashboardLayout>
-          <DashboardPage />
-        </DashboardLayout>
-      ) : (
-        <LandingLayout>
-          <LandingPage />
-        </LandingLayout>
-      )}
+      <LandingLayout>
+        <LandingPage />
+      </LandingLayout>
     </main>
   );
 }
